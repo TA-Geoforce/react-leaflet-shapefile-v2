c2a06d7c134f3d92283ddd8bb80abd4e3189e56d
Mylothros-Charalampos <charalampos.mylothros@navd.ai>
2021-06-24 15:48:33 +0300

    now you can pass all the props of GeoJSON more dynamically not only style and onEachFeature

--- src/ShapeFile.js
+++ src/ShapeFile.js
@@ -3,26 +3,23 @@ import { GeoJSON } from 'react-leaflet'
 import shp from 'shpjs'
 
 function ShapeFile(props) {
-  const [ data, setData ] = useState(null)
-
-  const geoJSONProps= {
-    style: props.style,
-    onEachFeature: props.onEachFeature
-  }
+  const [ geoJSONData, setGeoJSONData ] = useState(null)
+  
+  const { data, ...geoJSONProps} = props
 
   useEffect(() => {
       props.data.onload = function(buffer) {
           shp(buffer.target.result).then(function(dataConverted){
-              setData(dataConverted) 
+            setGeoJSONData(dataConverted) 
           }) 
       } 
   },[])
 
  return (
     <div>
-        {data !== null ? 
+        {geoJSONData !== null ? 
           <div>
-            <GeoJSON data = {data} {...geoJSONProps} />
+            <GeoJSON data = {geoJSONData} {...geoJSONProps} />
           </div>
         :null}
     </div>
