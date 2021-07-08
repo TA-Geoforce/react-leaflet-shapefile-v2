3848a0324fda04e5904fd29619a33f5be56e789b
Mylothros-Charalampos <69667373+Mylothros-Charalampos@users.noreply.github.com>
2021-07-08 16:07:52 +0300

    fix: changed the path for npm to be lib
    fix: changed the path for npm to be lib

--- example/shapefile.js
+++ example/shapefile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MapContainer, TileLayer, LayersControl } from 'react-leaflet'
-import { ShapeFile } from '../src/index.js'
+import { ShapeFile } from '../lib/index.js'
 
 const { BaseLayer, Overlay } = LayersControl;
 
